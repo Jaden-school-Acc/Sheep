0ba2483fabf5a59e62e0fc7191a0e1a450235d21
Jaden-school-Acc <jwilliams0691@langleyschools.ca>
2024-12-18 15:05:52 -0800

    Add DoorHandler And Closing
    Fix Room Bug where rooms weren't all connected between hallways.

---
+++ Sheep/Assets/Game/Scripts/Office/Rooms/RoomHandler.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomHandler : MonoBehaviour
+{
+    public Room[] rooms;
+    public DoorVis[] doorsVis;
+}
